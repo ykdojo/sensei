1293bba71b6b73da3c7e4e7f8bcbd4cda431f846
ykdojo <yksu4p@gmail.com>
2023-02-06 01:23:03 -0800

    Set up bin

--- sensei.js
+++ sensei.js
@@ -1,11 +1,12 @@
+#!/usr/bin/env node
 const prompt = require('prompt-sync')()
 const call_davinci = require('./call_davinci')
 
 async function sensei() {
   console.log("yo I'm a bot")
-  console.log("anything you want to say to me?")
-  const user_input = prompt("")
-  const result = await call_davinci(user_input)
-  console.log(result)
+  // console.log("anything you want to say to me?")
+  // const user_input = prompt("")
+  // const result = await call_davinci(user_input)
+  // console.log(result)
 }
 sensei()
