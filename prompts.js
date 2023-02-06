d3bde0e263caa8343b3ef7b1bec62faa100931d7
ykdojo <yksu4p@gmail.com>
2023-02-06 10:52:45 -0800

    prompt importing

---
+++ prompts.js
@@ -0,0 +1,4 @@
+const prompt1 = 'p1'
+const prompt2 = 'p2'
+
+module.exports = {prompt1, prompt2}
--- sensei.js
+++ sensei.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 const prompt = require('prompt-sync')()
 const call_davinci = require('./call_davinci')
+const {prompt1, prompt2} = require('./prompts')
 
 async function sensei() {
   console.log("yo I'm a bot")
+  // console.log(prompt1)
+  // console.log(prompt2)
   // console.log("anything you want to say to me?")
   // const user_input = prompt("")
   // const result = await call_davinci(user_input)
