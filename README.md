8b1812792397d384ad6592f18cdbf7dc16e363ea
ykdojo <yksu4p@gmail.com>
2023-02-06 19:13:20 -0800

    Start handling flags and take user input

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # How to use it
 
+## Set this up
+
+- Step 1: Copy .env.example to .env and replace the OpenAI key (you'll need to sign up)
+- Step 2: Run `npm install -g .`
+- Step 3: Run `sensei`
+
 ## Find a command
 
 sensei -c revert back to a particular commit with git
@@ -10,10 +16,22 @@ sensei -c npm install a package from the current working directory
 
 ## Explain a command
 
-sensei -e git reset --hard <commit-hash>
+sensei -e
 
-## Ask anything
+Enter a command to explain:
+
+git reset --hard <commit-hash>
+
+## Ask anything (TODO: use Codex?)
 
 sensei -a how to install multiple versions of node
 
 sensei -a how to resolve a merge conflict
+
+sensei -a
+
+Ask sensei anything: how to check if a member exists in a JS object
+
+sensei -a
+
+Ask sensei anything: syntax for error handling in JS
--- sensei.js
+++ sensei.js
@@ -1,22 +1,34 @@
 #!/usr/bin/env node
 const call_davinci = require('./call_davinci')
 const {findCommandTemplate, explainCommandTemplate, askQuestionTemplate} = require('./prompts')
+const promptSync = require('prompt-sync')()
 
-const description = 'revert back to a particular commit with git'
-const findCommandPrompt = findCommandTemplate.replace('<use case>', description)
+const yargs = require("yargs");
 
-const command = 'git reset --hard <commit-hash>'
-const explainCommandPrompt = explainCommandTemplate.replace('<command>', command)
-
-const question = `About the following command: "git reset --hard <commit-hash>", is there a way to achieve the same thing in a way that's reversible?`
-const askQuestionPrompt = askQuestionTemplate.replace('<question>', question)
-
-// TODO: ask a follow-up question?
-const prompt = askQuestionPrompt
+const options = yargs
+ .usage("Usage: -e or -a")
+ .option("e", { alias: "explain", describe: "Explain a terminal command", type: "string", demandOption: false })
+ .option("a", { alias: "ask", describe: "Ask anything", type: "string", demandOption: false })
+ .argv;
 
 async function sensei() {
-  console.log(prompt)
-  const result = await call_davinci(prompt)
+  let prompt
+
+  // TODO: When none of the correct flags are selected, explain the flags.
+  if (options.hasOwnProperty('a')) {
+    console.log("Ask sensei anything: ")
+    const question = promptSync("")
+    const askQuestionPrompt = askQuestionTemplate.replace('<question>', question)
+    prompt = askQuestionPrompt
+  }
+
+  let result
+  try {
+    // console.log('Calling Davinci')
+    result = await call_davinci(prompt)
+  } catch(e) {
+    console.log('Encountered an error! Please try again.')
+  }
   console.log(result)
 }
 sensei()
